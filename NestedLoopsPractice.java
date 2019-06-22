812177225bebdf6770a51838a971fe6341e9209e
tgeorge1001 <51996040+tgeorge1001@users.noreply.github.com>
2019-06-22 15:15:01 -0400

    Update NestedLoopsPractice.java

--- NestedLoopsPractice.java
+++ NestedLoopsPractice.java
@@ -11,7 +11,8 @@ public class NestedLoopsPractice {
  */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("How many levels should the staircase have?");
+		//User input to how many levels the pyramid should have
+		System.out.println("How many levels should the pyramid have?");
 		int numLevels = in.nextInt();
 		for (int i = 0; i < numLevels; i++) {
 			for (int j = 0; j <= i; j++) {
